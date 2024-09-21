94e106f09adedd08f8c32d2eee89faa6a9c23ad7
AkiraBrown <akira.watson.brown@gmail.com>
2024-09-20 23:19:46 -0400

    feat(main): first Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# URL Shortener
+
+This project is a URL shortener done isn GO for my own learning
---
+++ handler.go
@@ -0,0 +1,12 @@
+package urlshortener
+
+import "net/http"
+
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return nil
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error){
+	return nil,nil
+}
